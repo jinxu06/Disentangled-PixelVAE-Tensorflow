f471acaa68ef357caa391c0408e80f4670f4172a
Jin Xu <aaron.jin.xu@gmail.com>
2018-06-18 18:12:03 +0800

    sync.sh

--- run_cifar10.py
+++ run_cifar10.py
@@ -88,8 +88,8 @@ if args.debug:
     test_data = data_set.test(shuffle=False, limit=-1)
 else:
     train_data = data_set.train(shuffle=True, limit=-1)
-    eval_data = test_data #data_set.train(shuffle=True, limit=batch_size*10)
-    test_data = data_set.test(shuffle=False, limit=-1)
+    eval_data = data_set.test(shuffle=False, limit=-1)
+    test_data = eval_data 
 
 # masks
 if "output" not in args.use_mask_for:
